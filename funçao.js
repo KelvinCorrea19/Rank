64116f1f1ac79b2039b65364b5393ac1eb79532b
KelvinCorrea19 <kelvin.correa@hotmail.com>
2024-11-03 22:39:36 -0300

    calculadora

---
+++ funçao.js
@@ -0,0 +1,29 @@
+function somar(a, b) {
+    return a + b; // Retorna a soma de a e b
+}
+
+let entrada1 = 2; // Primeira entrada
+let entrada2 = 3; // Segunda entrada
+let resultado = somar(entrada1, entrada2); // Chama a função somar
+
+console.log("Posicao final do heroi:", resultado);
+
+function somar(a, b) {
+    return a + b; // Retorna a soma de a e b
+}
+
+let entrada3 = 15; // Primeira entrada
+let entrada4 = 3; // Segunda entrada
+let resultadoo = somar(entrada3, entrada4); // Chama a função somar
+
+console.log("Posicao final do heroi:", resultadoo);
+function somar(a, b) {
+    return a + b; // Retorna a soma de a e b
+}
+
+let entrada5 = 15; // Primeira entrada
+let entrada6 = 3; // Segunda entrada
+let resultadooo = somar(entrada5, entrada6); // Chama a função somar
+
+console.log("Posicao final do heroi:", resultadooo);
+
